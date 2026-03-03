39258ecb0983c3c7cbe90ec9cfb90c069ccf4d08
Roboto63 <sergedora80@gmail.com>
2026-03-03 11:07:39 -0600

    feat: separar analítica WhatsApp en página independiente /promotor/whatsapp/analitica

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import {
   LayoutDashboard, Users, Settings, ClipboardList, Building2,
   Map, Building, UsersRound, Briefcase, Filter, TrendingUp,
-  Inbox, Scale, ArrowLeftRight, ChevronLeft, Menu,
+  Inbox, Scale, ArrowLeftRight, ChevronLeft, Menu, BarChart3,
 } from "lucide-react";
 import { Tooltip } from "@/components/ui/Tooltip";
 import { ReactNode } from "react";
@@ -124,6 +124,7 @@ function SidebarContent({
           <>
             <NavItem label="Mi Asignación" href="/promotor/oportunidades" icon={<TrendingUp className="w-5 h-5" />} pathname={pathname} showLabels={showLabels} />
             <NavItem label="WhatsApp" href="/promotor/whatsapp" icon={<WhatsAppIcon />} pathname={pathname} showLabels={showLabels} exact />
+            <NavItem label="Analítica WA" href="/promotor/whatsapp/analitica" icon={<BarChart3 className="w-5 h-5" />} pathname={pathname} showLabels={showLabels} exact />
             <NavItem label="Configuración" href="/promotor/configuracion" icon={<Settings className="w-5 h-5" />} pathname={pathname} showLabels={showLabels} exact />
           </>
         ) : (
