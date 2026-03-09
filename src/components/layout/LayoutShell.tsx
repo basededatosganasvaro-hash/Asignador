96c9eff3ee456933a8679bed504e53fc96d6072b
Roboto63 <sergedora80@gmail.com>
2026-03-09 15:03:24 -0600

    fix: contener overflow en LayoutShell para cards fijos con scroll horizontal

--- src/components/layout/LayoutShell.tsx
+++ src/components/layout/LayoutShell.tsx
@@ -10,9 +10,9 @@ export default function LayoutShell({ rol, children }: { rol: string; children: 
   return (
     <div className="flex min-h-screen bg-base">
       <Sidebar rol={rol} open={open} onToggle={toggle} />
-      <div className="flex-1 flex flex-col" style={{ marginLeft: DRAWER_COLLAPSED }}>
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden" style={{ marginLeft: DRAWER_COLLAPSED }}>
         <Header />
-        <main className="flex-1 p-6">
+        <main className="flex-1 p-6 min-w-0 overflow-auto">
           {children}
         </main>
       </div>
